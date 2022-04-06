c79e8e35c2b51761c5d988404caf2192d52cb541
jenna20118 <102357191+jenna20118@users.noreply.github.com>
2022-04-06 03:27:36 +0300

    Combine dao files

--- VaalikoneApp/src/main/java/app/CandidateQuestions.java
+++ VaalikoneApp/src/main/java/app/CandidateQuestions.java
@@ -10,8 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import dao.AnswerDao;
-import dao.QuestionDao;
+import dao.Dao;
 import data.Answer;
 import data.Question;
 
@@ -21,17 +20,14 @@ import data.Question;
 @WebServlet("/candidatequestions")
 public class CandidateQuestions extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private QuestionDao questionDao=null;
-	private AnswerDao answerDao=null;
-	private String candidateId = "5";
+	private Dao dao = null;
+	private String candidateId = "6";
 	
 	@Override
 	public void init() {
-		questionDao=new QuestionDao("jdbc:mysql://localhost:3306/vaalikone", "root", "root");
-		answerDao=new AnswerDao("jdbc:mysql://localhost:3306/vaalikone", "root", "root");
+		dao=new Dao();
 	}
 	 
-	
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -59,32 +55,29 @@ public class CandidateQuestions extends HttpServlet {
 		}
 		
 		ArrayList<Question> questionList=null;
-		if (questionDao.getConnection()) {
-			questionList=questionDao.readAllQuestions();
-		}
-		else {
-			System.out.println("No connection to database");
+		questionList=dao.readAllQuestions();
+		
+		if (questionList == null) {
+			response.getWriter().println("Couldn't get questions");
 		}
 		
-		for (int i = 0; i<questionList.size(); i++) {
-			int questionId = questionList.get(i).getId();
-			
-			if (questionId == Integer.parseInt(question)) {
-				request.setAttribute("question", questionList.get(i));
-				Answer candidateAnswer = null;
+		else {
+			for (int i = 0; i<questionList.size(); i++) {
+				int questionId = questionList.get(i).getId();
 				
-				if (answerDao.getConnection()) {
-					candidateAnswer = answerDao.readAnswer(candidateId, Integer.toString(questionId));
-				} else {
-					System.out.println("No connection to database");
+				if (questionId == Integer.parseInt(question)) {
+					request.setAttribute("question", questionList.get(i));
+					Answer candidateAnswer = null;
+					
+					candidateAnswer = dao.readAnswer(candidateId, Integer.toString(questionId));
+					
+					if (candidateAnswer == null) {
+						RequestDispatcher rd=request.getRequestDispatcher("/jsp/addanswerform.jsp");		
+						rd.forward(request, response);	
+					} else {
+						response.getWriter().append("Question already answered");
+					}			
 				}
-				
-				if (candidateAnswer == null) {
-					RequestDispatcher rd=request.getRequestDispatcher("/jsp/addanswerform.jsp");		
-					rd.forward(request, response);	
-				} else {
-					response.getWriter().append("Question already answered");
-				}			
 			}
 		}
 	}
@@ -92,29 +85,19 @@ public class CandidateQuestions extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		if (answerDao.getConnection()) {			
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
 			String questionId = request.getParameter("questionId");
 			String answer = request.getParameter("answer");
 				
-				if (candidateId == null) {
-					response.getWriter().println("Candidate id is missing");
-				} else if (questionId == null) {
-					response.getWriter().println("Question id is missing");
-				} else  {
-					if (answer != null) {
-						try {
-							answerDao.insertAnswer(candidateId, questionId, answer);
-							doGet(request,response);
-						} catch (Exception e) {
-							response.getWriter().println("An error occurred");
-							System.out.println(e);
-						}						
-					}
+			if (candidateId == null) {
+				response.getWriter().println("Candidate id is missing");
+			} else if (questionId == null) {
+				response.getWriter().println("Question id is missing");
+			} else  {
+				if (answer != null) {
+					dao.insertAnswer(candidateId, questionId, answer);
+					doGet(request,response);					
 				}
-		}
-		else {
-			System.out.println("No connection to database");
-		}
+			}
 	}
 }
